aca0ff8176edc0c5c2da866c2bcbda62672d1ec5
Kamran Ali <ely.kamran@gmail.com>
2025-04-09 15:44:49 -0400

    services

---
+++ Services/BookManager.cs
@@ -0,0 +1,98 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Exceptions;
+using Entities.LinkModels;
+using Entities.Models;
+using Entities.RequestFeatures;
+using Repositories.Contracts;
+using Services.Contracts;
+using System.Dynamic;
+using static Entities.Exceptions.BadRequestException;
+
+namespace Services
+{
+    public class BookManager : IBookService
+    {
+        private readonly IRepositoryManager _manager;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+        private readonly IBookLinks _bookLinks;
+
+        public BookManager(IRepositoryManager manager, ILoggerService logger, IMapper mapper, IBookLinks bookLinks)
+        {
+            _manager = manager;
+            _logger = logger;
+            _mapper = mapper;
+            _bookLinks = bookLinks;
+        }
+
+        private async Task<Book> GetOneBookByIdAndCheckExists(int id, bool trackChanges)
+        {
+            var entity = await _manager.Book.GetOneBookbyIdAsync(id, trackChanges);
+            if (entity is null)
+                throw new BookNotFoundException(id);
+            return entity;
+        }
+
+        public async Task<BookDto> CreateOneBookAsync(BookDtoForInsertion bookDto)
+        {
+            var entity = _mapper.Map<Book>(bookDto);
+            _manager.Book.CreateOneBook(entity);
+            await _manager.SaveAsync();
+            return _mapper.Map<BookDto>(entity);
+        }
+
+        public async Task DeleteOneBookAsync(int id, bool trackChanges)
+        {
+            var entity = await GetOneBookByIdAndCheckExists(id, trackChanges);
+            _manager.Book.DeleteOneBook(entity);
+            await _manager.SaveAsync();
+        }
+
+        public async Task<(LinkResponse linkResponse, MetaData metaData)> GetAllBooksAsync(LinkParameters linkParameters, bool trackChanges)
+        {
+            if (!linkParameters.BookParameters.ValidPriceRange)
+                throw new InvalidPriceFiltersBadRequestException();
+
+            var booksWithMetaData = await _manager.Book.GetAllBooksAsync(linkParameters.BookParameters, trackChanges);
+
+            var booksDto = _mapper.Map<IEnumerable<BookDto>>(booksWithMetaData);
+            var links = _bookLinks.TryGenerateLinks(booksDto, linkParameters.BookParameters.Fields, linkParameters.HtppContext);
+
+            return (linkResponse: links, metaData: booksWithMetaData.MetaData);
+        }
+
+        public async Task<BookDto> GetOneBookByIdAsync(int id, bool trackChanges)
+        {
+            var entity = await GetOneBookByIdAndCheckExists(id, trackChanges);
+            return _mapper.Map<BookDto>(entity);
+        }
+
+        public async Task<(BookDtoForUpdate bookDtoForUpdate, Book book)> GetOneBookForPatchAsync(int id, bool trackChanges)
+        {
+            var entity = await GetOneBookByIdAndCheckExists(id, trackChanges);
+            var bookDtoForUpdate = _mapper.Map<BookDtoForUpdate>(entity);
+            return (bookDtoForUpdate, entity);
+        }
+
+        public async Task SaveChangesForPatchAsync(BookDtoForUpdate bookDtoForUpdate, Book book)
+        {
+            _mapper.Map(bookDtoForUpdate, book);
+            await _manager.SaveAsync();
+        }
+
+        public async Task UpdateOneBookAsync(int id, BookDtoForUpdate bookDto, bool trackChanges)
+        {
+            var entity = await GetOneBookByIdAndCheckExists(id, trackChanges);
+            entity = _mapper.Map<Book>(bookDto);
+            _manager.Book.Update(entity);
+            await _manager.SaveAsync();
+        }
+
+        public async Task<List<Book>> GetAllBooksAsync(bool trackChanges)
+        {
+            var books = await _manager.Book.GetAllBooksAsync(trackChanges);
+            return books;
+        }
+    }
+}
---
+++ Services/ServiceManager.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Repositories.Contracts;
+using Services.Contracts;
+
+namespace Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IBookService> _bookService;
+        public ServiceManager(IRepositoryManager repositoryManager, ILoggerService logger, IMapper mapper, IBookLinks bookLinks)
+        {
+            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper, bookLinks));
+        }
+        public IBookService BookService => _bookService.Value;
+    }
+}
